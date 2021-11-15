b18b98236b8246611f28e3972d3cdf03530d7f83
jcarbaugh7 <carbaugh@bu.edu>
2021-11-14 22:04:06 -0500

    Data Cleaning

--- dataWrangling.R
+++ dataWrangling.R
@@ -6,15 +6,8 @@ library(stringi)
 data <- read_csv("CFBeattendance.csv",)
 str(data)
 
-#'To do:
-#'1.Turn date and 'time' into time variables (Completed)
-#'2.Clean opponent string. Add new column for "In Conference" (Completed)
-#'3.Turn rank into factor ?
-#'4.Clean up site string, additional column for "big game" data (Completed)
-#'5.Clean up result string, split into different columns
-#'
-
 data %<>% mutate(Date = strptime(Date, format = "%m/%d/%Y"))
+data %<>% mutate(Date = strptime(Time, format = "%H:%M %p"))
 
 data %<>% mutate(Opponent = stri_enc_toutf8(Opponent))
 
@@ -35,5 +28,14 @@ data %<>% separate(col= Site,
                    into = c("Site", "BigGameName"),
                    sep = " \\(",
                    fill = "right") %>%
-  mutate(BigGame = ifelse(is.na(BigGameName), 0, 1))
+  mutate(BigGame = ifelse(is.na(BigGameName), 0, 1)) %>%
+  mutate(BigGameName = str_sub(BigGameName, 1, -2))
+
+names(data)<-make.names(names(data),unique = TRUE)
+
+data %<>% mutate(GamesPlayed = Current.Wins + Current.Losses)
+data %<>% mutate(Record = ifelse(Current.Wins + Current.Losses ==0, 0, Current.Wins/(GamesPlayed)))
+
+#Needs some switching
+sum(data$TMAX <= data$TMIN)
 
