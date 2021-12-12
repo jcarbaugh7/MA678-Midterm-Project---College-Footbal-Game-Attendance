0c8713b220d3d65d37e6caac91387023141617a2
jcarbaugh7 <carbaugh@bu.edu>
2021-12-12 11:26:56 -0500

    Final Report

--- dataWrangling.R
+++ dataWrangling.R
@@ -1,18 +1,16 @@
-library(tidyverse)
-library(magrittr)
-library(stringr)
-library(stringi)
-library(arm)
-library(rstan)
-library(rstanarm)
-library(lattice)
-
-library(lme4)
-
-#https://geoffboeing.com/2016/09/college-football-stadium-attendance/
+# library(tidyverse)
+# library(magrittr)
+# library(stringr)
+# library(stringi)
+# # library(arm)
+# # library(rstan)
+# library(rstanarm)
+# library(lattice)
+# library(coefplot)
+# 
+# library(lme4)
 
 data <- read_csv("CFBeattendance.csv")
-str(data)
 
 data %<>% mutate(Time = strptime(Time, format = "%I:%M %p"))
 data %<>% mutate(Date = strptime(Date, format = "%m/%d/%Y"))
@@ -44,12 +42,16 @@ names(data)<-make.names(names(data),unique = TRUE)
 
 data %<>% mutate(GamesPlayed = Current.Wins + Current.Losses)
 
-data %<>% mutate(Record = ifelse(Current.Wins + Current.Losses ==0, 0, Current.Wins/(GamesPlayed)))
+data %<>% mutate(Record = ifelse(Current.Wins + Current.Losses ==0, 1, Current.Wins/(GamesPlayed)))
 
 data %<>% mutate(Opponent_Rank = factor(Opponent_Rank, levels = c("1", "2", "3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","23","24","25","NR")))
 data %<>% mutate(Rank = factor(Rank, levels = c("1", "2", "3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","23","24","25","NR")))
 
+data %<>% mutate(Month_f = factor(Month, levels = c(8,9,10,11,12,1,4)))
+
 data %<>% mutate(New.Coach = as.integer(New.Coach), Tailgating = as.integer(Tailgating))
+data %<>% mutate(did_rain = ifelse(PRCP >0, 1,0), did_snow = ifelse(SNOW >0, 1,0))
+
 
 needs_switch <- which(data$TMAX <= data$TMIN)
 for (row in needs_switch){
@@ -58,79 +60,78 @@ for (row in needs_switch){
   data[row,22] <- temp
 }
 
+scale <- 1/(max(data$Fill.Rate) - min(data$Fill.Rate)) - .001
+test <- scale * data$Fill.Rate
+test <- test - min(test) + .001
+data_t <- mutate(data,Fill_Fixed = test)
+
 
 #################################################################
 #Visualization(EDA)
 
-data_bc <- filter(data, Team == "Notre Dame")
-plot(x = data_bc$Date, y = data_bc$Fill.Rate)
-ggplot(data) + geom_line(mapping = aes(x=as.Date(Date), y=Fill.Rate, color = Team), show.legend = FALSE)
-
-ggplot(data, aes(x = PRCP, y = Fill.Rate)) + geom_point()
-
-ggplot(data, aes(x = SNOW, y = Fill.Rate)) + geom_point()
-
-ggplot(data_bc, aes(x = TMAX, y = Fill.Rate)) + geom_point()
-
-ggplot(data, aes(x = Rank, y = Fill.Rate)) + geom_point()
-
-ggplot(data, aes(x = Record, y = Fill.Rate)) + geom_point()
-ggplot(data_bc, aes(x = GamesPlayed, y = Fill.Rate)) + geom_point()
-
-mean(filter(data, BigGame == 0)$Fill.Rate)
-mean(filter(data, BigGame == 1)$Fill.Rate)
-
-mean(filter(data, NonConference == 0)$Fill.Rate)
-mean(filter(data, NonConference == 1)$Fill.Rate)
-
-mean(filter(data, CollegeGameDay == 0)$Fill.Rate)
-mean(filter(data, CollegeGameDay == 1)$Fill.Rate)
-
-mean(filter(data, Tailgating == 0)$Fill.Rate)
-mean(filter(data, Tailgating == 1)$Fill.Rate)
-
-mean(filter(data, New.Coach == 0)$Fill.Rate)
-mean(filter(data, New.Coach == 1)$Fill.Rate)
-
-length(filter(data, Team == "Clemson")$Team)
-
-ggplot(data)+geom_density(alpha=0.3)+
-  aes(x=qlogis(Fill.Rate),color=Team)+facet_wrap(~Team)+theme(legend.position="none")+geom_rug()+
-  xlab("Stadium Fill Rate (Percentage")+geom_vline(xintercept=mean(data$Fill.Rate),color="red",lty=2)
-
-ggplot(data)+geom_line()+
-  aes(x=as.Date(Date), y = Fill.Rate, color=Team)+facet_wrap(~Team)+theme(legend.position="none")+
-  xlab("Stadium Fill Rate (Percentage")
-
-ggplot(data)+geom_histogram()+
-  aes(x = Fill.Rate, fill=Team, color = Team)+facet_wrap(~Team)+theme(legend.position="none")+
-  xlab("Stadium Fill Rate (Percentage")
-
-ggplot(data)+geom_histogram()+
-  aes(x = Fill.Rate, fill=Conference, color = Conference)+facet_wrap(~Conference)+theme(legend.position="none")+
-  xlab("Stadium Fill Rate (Percentage")
-
-ggplot(data)+geom_jitter()+
-  aes(x=BigGame, y = Fill.Rate, color=Team)+facet_wrap(~Team)+theme(legend.position="none")+
-  xlab("Stadium Fill Rate (Percentage")
-
-ggplot(data)+geom_jitter()+
-  aes(x=NonConference, y = Fill.Rate, color=Team)+facet_wrap(~Team)+theme(legend.position="none")+
-  xlab("Stadium Fill Rate (Percentage")
-
-
-mean_fillrates <- data %>% group_by(Team) %>%
-  summarise(Average_FillRate = mean(Fill.Rate, na.rm = TRUE)) %>%
-  arrange(desc(Average_FillRate))
-
-
-
-ggplot(mean_fillrates) +
-  geom_bar(stat = "identity", mapping = aes(x = reorder(Team, -Average_FillRate), y = Average_FillRate, fill = Team), show.legend = FALSE) + 
-  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
-
-
-hist(data$Fill.Rate)
+# data_bc <- filter(data, Team == "Boston College")
+# plot(x = data_bc$Date, y = data_bc$Fill.Rate)
+# ggplot(data) + geom_line(mapping = aes(x=as.Date(Date), y=Fill.Rate, color = Team), show.legend = FALSE)
+# 
+# ggplot(data, aes(x = PRCP, y = Fill.Rate)) + geom_point()
+# 
+# ggplot(data, aes(x = SNOW, y = Fill.Rate)) + geom_point()
+# 
+# ggplot(data_bc, aes(x = TMAX, y = Fill.Rate)) + geom_point()
+# 
+# ggplot(data, aes(x = Rank, y = Fill.Rate)) + geom_point()
+# 
+# ggplot(data, aes(x = Record, y = Fill.Rate)) + geom_point()
+# ggplot(data_bc, aes(x = GamesPlayed, y = Fill.Rate)) + geom_point()
+# 
+# mean(filter(data, BigGame == 0)$Fill.Rate)
+# mean(filter(data, BigGame == 1)$Fill.Rate)
+# 
+# mean(filter(data, NonConference == 0)$Fill.Rate)
+# mean(filter(data, NonConference == 1)$Fill.Rate)
+# 
+# mean(filter(data, CollegeGameDay == 0)$Fill.Rate)
+# mean(filter(data, CollegeGameDay == 1)$Fill.Rate)
+# 
+# mean(filter(data, Tailgating == 0)$Fill.Rate)
+# mean(filter(data, Tailgating == 1)$Fill.Rate)
+# 
+# mean(filter(data, New.Coach == 0)$Fill.Rate)
+# mean(filter(data, New.Coach == 1)$Fill.Rate)
+# 
+# ggplot(data)+geom_density(alpha=0.3)+
+#   aes(x=qlogis(Fill.Rate),color=Team)+facet_wrap(~Team)+theme(legend.position="none")+geom_rug()+
+#   xlab("Stadium Fill Rate (Percentage")+geom_vline(xintercept=mean(data$Fill.Rate),color="red",lty=2)
+# 
+# ggplot(data)+geom_line()+
+#   aes(x=as.Date(Date), y = Fill.Rate, color=Team)+facet_wrap(~Team)+theme(legend.position="none")+
+#   xlab("Stadium Fill Rate (Percentage")
+# 
+# ggplot(data)+geom_histogram()+
+#   aes(x = Fill.Rate, fill=Team, color = Team)+facet_wrap(~Team)+theme(legend.position="none")+
+#   xlab("Stadium Fill Rate (Percentage")
+# 
+# ggplot(data)+geom_histogram()+
+#   aes(x = Fill.Rate, fill=Conference, color = Conference)+facet_wrap(~Conference)+theme(legend.position="none")+
+#   xlab("Stadium Fill Rate (Percentage")
+# 
+# ggplot(data)+geom_jitter()+
+#   aes(x=BigGame, y = Fill.Rate, color=Team)+facet_wrap(~Team)+theme(legend.position="none")+
+#   xlab("Stadium Fill Rate (Percentage")
+# 
+# ggplot(data)+geom_jitter()+
+#   aes(x=NonConference, y = Fill.Rate, color=Team)+facet_wrap(~Team)+theme(legend.position="none")+
+#   xlab("Stadium Fill Rate (Percentage")
+# 
+# ggplot(mean_fillrates) +
+#   geom_bar(stat = "identity", mapping = aes(x = reorder(Team, -Average_FillRate), y = Average_FillRate, fill = Team), show.legend = FALSE) + 
+#   theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
+# 
+# 
+# 
+# 
+# 
+# hist(data$Fill.Rate)
 
 # hist(qlogis(data$Fill.Rate-min(data$Fill.Rate) + .01))
 # qqnorm(qlogis(data$Fill.Rate-min(data$Fill.Rate) + .01), pch = 1, frame = FALSE)
@@ -146,37 +147,33 @@ hist(data$Fill.Rate)
 # qqnorm(log(-log(test)), pch = 1, frame = FALSE)
 # qqline(log(-log(test)), col = "steelblue", lwd = 2)
 
-ggplot(data)+
-  aes(TMAX, Fill.Rate)+
-  geom_point(aes(color = Team),alpha = 0.1)+
-  labs(title="Maximum Temperature vs. Stadium Fill Rate, Stratified over Tailgating Quality",x="Max Temperature (F)",y="Fill Rate (%)")+
-  geom_smooth(aes(color = Team),method = "lm",se = FALSE)+theme(legend.position="none")+
-  facet_wrap(~Tailgating)
-
-
 #################################################################
 #Model Selection
+# 
+# 
+# hist(data_t$Fill_Fixed)
+# hist(data$PRCP)
+# 
+# hist(qlogis(data_t$Fill_Fixed))
+# #gamesPlayed accounts for temp, month, nonconf
+# 
+# fit4 <- lmer(qlogis(Fill_Fixed)~ BigGame + Record + Tailgating + TMAX + GamesPlayed + NonConference + PRCP + (1+Record|Team),data_t)
+# USE FIT5
 
-lin_fit <- stan_glm(Fill.Rate ~ Tailgating + PRCP + TMIN + NonConference + BigGame + Record, data = data, refresh = 0)
-rbind(lin_fit$coefficients,lin_fit$ses)
+fit5 <- lmer(qlogis(Fill_Fixed)~ BigGame + Tailgating * Record + GamesPlayed * TMIN + NonConference + PRCP + (1 + Record|Team),data_t)
 
-multi_fit <- lmer(Fill.Rate ~ Tailgating + PRCP + TMIN + NonConference + BigGame + Record + (1+TMIN|Team), data)
+# summary(fit4)
+# summary(fit5)
+# plot(fit4)
+# plot(fit5)
+# qqmath(fit4)
+# qqmath(fit5)
+# 
+# fixef(fit5)
+# ranef(fit5)
+# 
+# coefplot(fit4)
+# coefplot(fit5)
+# dotplot(ranef(fit5, condVar =TRUE))
+# 
 
-
-scale <- 1/(max(data$Fill.Rate) - min(data$Fill.Rate)) - .001
-test <- scale * data$Fill.Rate
-range(test)
-test <- test - min(test) + .001
-data_t <- mutate(data,Fill_Fixed = test)
-hist(qlogis(test))
-
-hist(data$GamesPlayed)
-
-fit4 <- lmer(qlogis(Fill_Fixed)~TMAX+GamesPlayed+Tailgating + BigGame + (1+TMAX|Team)+(1+Tailgating|Team),data_t)
-fit5 <- lmer(qlogis(Fill_Fixed)~TMAX+Tailgating + BigGame + Month + (1+TMAX|Team)+(1+Record|Team)+(1+Tailgating|Team) +(1+Month|Team) ,data_t)
-
-summary(fit5)
-plot(fit4)
-plot(fit5)
-qqmath(fit4)
-qqmath(fit5)
---
+++ visualization.R
@@ -0,0 +1,44 @@
+
+
+scatter_line <- function(data){
+  names <- c(`0` = "Not Rated Top 25 in Tailgating", `1` = "Rated Top 25 in Tailgating")
+  
+  ggplot(data)+
+    aes(TMIN, Fill.Rate)+
+    geom_point(aes(color = Team),alpha = 0.1)+
+    labs(title="Minimum Temperature vs. Stadium Fill Rate, Stratified over Tailgating",x="Min Temperature (F)",y="Fill Rate (%)")+
+    geom_smooth(aes(color = Team),method = "lm",se = FALSE)+theme(legend.position="none")+
+    facet_wrap(~Tailgating, labeller = as_labeller(names))
+}
+
+
+record_bars <- function(data){
+  mean_record <- data %>% group_by(Team) %>%
+    summarise(Average_Record = mean(Record, na.rm = TRUE)) %>%
+    ungroup()
+  
+  tg <- select(data, Team, Tailgating)
+  tg <- tg[!duplicated(tg), ]
+  rec_tg <- inner_join(mean_record,tg) %>% mutate(Tailgating = as.factor(Tailgating))
+
+  ggplot(rec_tg) +
+    geom_bar(stat = "identity", mapping = aes(x = reorder(Team, -Average_Record), y = Average_Record, fill = Tailgating,), show.legend = TRUE) +
+    theme(axis.text.x = element_text(size = 6, angle = 90, vjust = 0.5, hjust=1))+ 
+    geom_hline(yintercept = mean(rec_tg$Average_Record))+ 
+    annotate("text", 55, mean(rec_tg$Average_Record), vjust = -1, label = "Overall Mean")+
+    labs(title="Average Team Records from 2000 to 2018",x="Team", y="Average Record (%)")
+}
+
+
+fixed <- function(fit){
+  coefplot(fit)
+}
+
+rand <- function(fit){
+  plot_model(fit, type="re",
+             vline.color="#A9A9A9", dot.size=1.5, value.offset=.2, sort.est = "Record")+
+    theme(axis.text.y = element_text(size = 6))
+}
+
+
+
